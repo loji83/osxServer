ffa135b66bd5286043fd345b53fcca7dbb70d6b5
Kang <Kang@ksmartech-ui-imac.local>
2015-10-06 10:16:11 +0900

    Initial Commit

---
+++ chatServer/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  chatServer
+//
+//  Created by Kang on 2015. 10. 6..
+//  Copyright © 2015년 Kang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
